ccf96bf204df1a2ba4187d4deacbbda80c885fe6
Daniel Mihalca <daniel.mihalca@outlook.com>
2023-11-30 21:05:26 +0200

    Basic search places implementation

--- modules/endpoint/src/SearchEndpoint.cpp
+++ modules/endpoint/src/SearchEndpoint.cpp
@@ -1,16 +1,25 @@
 #include "endpoint/SearchEndpoint.h"
 #include <drogon/HttpResponse.h>
 
+#include "service/SearchService.h"
+
 namespace endpoint
 {
+
+SearchEndpoint::SearchEndpoint(std::unique_ptr<service::SearchService>&& service)
+    : m_searchService(std::move(service))
+{
+}
 
 void SearchEndpoint::places(const drogon::HttpRequestPtr&                         request,
                             std::function<void(const drogon::HttpResponsePtr&)>&& callback,
                             const std::string&                                    queryText)
 {
-    auto response = drogon::HttpResponse::newHttpResponse();
-
-    callback(response);
+    m_searchService->searchPlaces(queryText, [callback](const std::string& response) {
+        auto resp = drogon::HttpResponse::newHttpResponse();
+        resp->setBody(response);
+        callback(resp);
+    });
 }
 
 }  // namespace endpoint
