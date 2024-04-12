b49829a61980a3125098be5e0b92098f6d9a2b9a
Daniel Mihalca <daniel.mihalca@outlook.com>
2024-04-12 09:50:53 +0300

    Updated main app to run a single search request for testing

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <string>
+
+#include "httpClientImpl/HttpClientImpl.h"
+#include "serviceImpl/SearchServiceProxyImpl.h"
+#include "serviceImpl/MapBoxPlacesPathBuilder.h"
 
 int main(int argc, char** argv)
 {
+    auto httpClient = std::make_unique<httpClientImpl::HttpClientImpl>("https://api.mapbox.com");
+
+    std::string accessToken = "";
+    auto        pathBuilder = std::make_unique<serviceImpl::MapBoxPlacesPathBuilder>(accessToken);
+
+    auto searchService =
+        std::make_unique<serviceImpl::SearchServiceProxyImpl>(std::move(httpClient), std::move(pathBuilder));
+
+
     std::cout << "Starting service..." << std::endl;
+
+    searchService->searchPlaces("kfc", "", [](const std::string&){});
+
     std::cout << "Ending service..." << std::endl;
 
     return 0;
